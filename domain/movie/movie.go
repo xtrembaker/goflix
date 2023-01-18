ebaba2bb7305000380aa9078b7f2d7d93a2ce3cb
xtrembaker <xtrembaker@gmail.com>
2023-01-18 18:11:28 +0100

    create db schema

--- domain/movie/movie.go
+++ domain/movie/movie.go
@@ -1,4 +1,18 @@
 package movie
 
+import (
+	"fmt"
+	"time"
+)
+
 type Movie struct {
+	ID          int64     `db:"id"`
+	Title       string    `db:"title"`
+	ReleaseDate time.Time `db:"release_date"`
+	Duration    int       `db:"duration"`
+	TrailerUrl  string    `db:"trailer_url"`
+}
+
+func (m Movie) String() string {
+	return fmt.Sprintf("id=%v, title=%v, releaseDate=%v, duration=%v, trailerUrl=%v", m.ID, m.Title, m.ReleaseDate, m.Duration, m.TrailerUrl)
 }
---
+++ infrastructure/persistence/sqlite/migrations.go
@@ -0,0 +1,20 @@
+package sqlite
+
+import "log"
+
+var schema = `
+CREATE TABLE IF NOT EXISTS movie
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+	title TEXT,
+	release_date TEXT,
+	duration INTEGER,
+	trailer_url TEXT
+)
+`
+
+// @TODO That would be great to be able to launch that in CLI (add a flag to do so)
+func ExecMigration(c *Client) {
+	c.db.MustExec(schema)
+	log.Println("Migration executed")
+}
